5fa77ed2a6a37dd1bd1c2ae771b764c140d1a407
nochnoenebo <gomenasai989@gmail.com>
2018-10-20 00:05:34 +0700

    урок 1.8

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,27 +2,38 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] agrs) {
-hello("world ");
+    public static void main(String[] agrs) {
+        hello("world ");
 
-double l = 5;
-System.out.println("Площадь квадрата" + area(l));
+        Square s = new Square();
+        s.l = 5;
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-double a = 4;
-double b = 6;
-    System.out.println("Площадь прямоугольника" + area(a, b));
-}
+        Rectangle r = new Rectangle();
+        r.a = 4;
+        r.b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + "=" + area(r));
+
+        Point p = new Point();
+        p.p1 = 1;
+        p.p2 = 2;
+        System.out.println("Расстояние между двумя точками с координатами " + p.p1 + " и " + p.p2 + " = " + area(p));
+    }
 
     public static void hello(String somebody) {
 
-    System.out.println("Hello" + somebody + "!");
-}
-
-    public static double area(double len){
-    return len*len;
+        System.out.println("Hello" + somebody + "!");
     }
 
-    public static double area(double a, double b) {
-        return a * b;
+    public static double area(Square s) {
+        return s.l * s.l;
+    }
+
+    public static double area(Rectangle r) {
+        return r.a * r.b;
+    }
+
+    public static double area(Point p) {
+        return p.p1 * p.p2;
     }
 }
